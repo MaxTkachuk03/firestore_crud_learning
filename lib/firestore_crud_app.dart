9f215f452c62b4d1d9d115ef39c4979e5128e388
MaxTkachuk03 <max200325622@gmail.com>
2024-10-08 16:15:33 +0300

    end of project

--- lib/firestore_crud_app.dart
+++ lib/firestore_crud_app.dart
@@ -1,5 +1,4 @@
 import 'package:firestore_crud_learning/provider/theme_provider.dart';
-import 'package:firestore_crud_learning/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
